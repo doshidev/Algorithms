588506f17aab48d41d366a383c3fa675b39a15ef
Devang Doshi <doshidev@gmail.com>
2017-01-08 16:44:45 -0800

    Insertion Sort

---
+++ InsertionSort.java
@@ -0,0 +1,35 @@
+/** This program does an insertion sort on an int array
+ * Created by Devang on 08-Jan-17.
+ */
+public class InsertionSort {
+    public static void main(String[] args){
+        int[] collection = {5, 8, 24, 15, 200, 192, 86, 35, 78, 4 ,9, 20};
+        printArray(collection);
+        insertionSort(collection, 'a');
+        printArray(collection);
+    }
+    
+    private static void insertionSort(int[] collection, char order){
+        if (order != 'a' && order != 'd') {
+            System.out.println("Incorrect order parameter! Use [a or d]");
+            return;
+        }
+        
+        for (int i = 0; i < collection.length; i++) {
+            int value = collection[i];
+            int j = i;
+            while(j > 0 && ((order =='a' && value < collection[j-1])||(order == 'd' && value > collection[j-1]))){
+                collection[j] = collection[j - 1];
+                j--;
+            }
+            collection[j] = value;
+        }
+    }
+    
+    private static void printArray(int[] collection){
+        for(int i = 0; i < collection.length; i++){
+            System.out.print("\t" + collection[i]);
+        }
+        System.out.println();
+    }
+}
---
+++ MergeSort.java
@@ -0,0 +1,7 @@
+package PACKAGE_NAME;
+
+/**
+ * Created by Devang on 08-Jan-17.
+ */
+public class MergeSort {
+}
